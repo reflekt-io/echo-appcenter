645e69ca14f780f27d3a831bbd7985d8ca522f70
determinedguy <mhd.athallah@gmail.com>
2021-11-27 16:21:00 +0700

    lol

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -73,11 +73,8 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              'Owkowkowk',
+              'Ntar ada lah ya',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
