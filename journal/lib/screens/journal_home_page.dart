33882c11ee95a23ca814536b87a051214a17c54a
determinedguy <mhd.athallah@gmail.com>
2021-12-24 03:55:37 +0700

    Make things neater

--- journal/lib/screens/journal_home_page.dart
+++ journal/lib/screens/journal_home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: constant_identifier_names
+// ignore_for_file: constant_identifier_names, avoid_print
 
 import 'dart:convert';
 import 'package:echo/common/network_service.dart';
