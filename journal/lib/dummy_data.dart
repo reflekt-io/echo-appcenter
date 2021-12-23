65ee7716a32ce80f30a36810de8b74dc79098551
determinedguy <mhd.athallah@gmail.com>
2021-12-24 03:31:49 +0700

    Change data

--- journal/lib/dummy_data.dart
+++ journal/lib/dummy_data.dart
@@ -2,19 +2,24 @@ import 'package:journal/models/journal.dart';
 import 'package:journal/models/journal_json.dart';
 
 // ignore: non_constant_identifier_names
-JournalJson DUMMY_CATEGORIES = JournalJson(
-  model: 'journal.journal',
-  pk: 1,
-  fields: [
-    Journal(
+List<JournalJson> DUMMY_CATEGORIES = [
+  JournalJson(
+    model: 'journal.journal',
+    pk: 1,
+    fields: Journal(
       user: 1,
       date: DateTime.parse('2021-11-13T09:50:25.151Z'),
-      feeling: 'marah,takut,stres,waspada,kewalahan,kesal,cemas,lesu,bosan,bingung',
+      feeling:
+          'marah,takut,stres,waspada,kewalahan,kesal,cemas,lesu,bosan,bingung',
       factor: 'pekerjaan,teman,pendidikan',
       anxietyRate: 9,
       summary: 'Aku sayang kesehatan mentalku :(',
     ),
-    Journal(
+  ),
+  JournalJson(
+    model: 'journal.journal',
+    pk: 2,
+    fields: Journal(
       user: 1,
       date: DateTime.parse('2021-11-13T07:45:13.151Z'),
       feeling: 'lega',
@@ -22,13 +27,16 @@ JournalJson DUMMY_CATEGORIES = JournalJson(
       anxietyRate: 4,
       summary: 'Enak banget makan :D',
     ),
-    Journal(
-      user: 1,
-      date: DateTime.parse('2021-11-13T05:59:59.151Z'),
-      feeling: 'antusias,gembira,semangat,bangga',
-      factor: 'tidur,bersantai,hiburan',
-      anxietyRate: 3,
-      summary: 'Tidurku nyenyak~',
-    )
-  ],
-);
+  ),
+  JournalJson(
+      model: 'journal.journal',
+      pk: 2,
+      fields: Journal(
+        user: 1,
+        date: DateTime.parse('2021-11-13T05:59:59.151Z'),
+        feeling: 'antusias,gembira,semangat,bangga',
+        factor: 'tidur,bersantai,hiburan',
+        anxietyRate: 3,
+        summary: 'Tidurku nyenyak~',
+      )),
+];
