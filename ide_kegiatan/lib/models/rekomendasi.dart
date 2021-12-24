1c97d79b0f53ca1231eae7c6fbce48359299387a
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2021-12-24 21:11:30 +0700

    Fix default rekomendasi

--- ide_kegiatan/lib/models/rekomendasi.dart
+++ ide_kegiatan/lib/models/rekomendasi.dart
@@ -1,9 +1,15 @@
 class Rekomendasi {
-  final String id;
   final String nama;
 
   const Rekomendasi({
-    required this.id,
     required this.nama,
   });
+
+  factory Rekomendasi.fromJson(Map<String, dynamic> json) => Rekomendasi(
+        nama: json["fields"]["nama"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "nama": nama,
+      };
 }
