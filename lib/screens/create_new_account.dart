ddff0ab92895a91913a7eec9318a48a44f9b015d
Muhammad Athallah <mhd.athallah@gmail.com>
2022-11-18 13:04:49 +0700

    Merge branch 'main' of https://github.com/reflekt-io/echo-appcenter into develop

--- lib/screens/create_new_account.dart
+++ lib/screens/create_new_account.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: constant_identifier_names
+// ignore_for_file: constant_identifier_names, use_build_context_synchronously
 
 import 'dart:convert' as convert;
 import 'package:echo/common/network_service.dart';
